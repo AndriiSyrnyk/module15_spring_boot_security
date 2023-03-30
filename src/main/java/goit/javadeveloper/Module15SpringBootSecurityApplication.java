166207a88e73e7323a6cb2fd57ff36af28bc8c60
Andrii Syrnyk <androso@ukr.net>
2023-03-30 19:27:21 +0300

    added all files

---
+++ src/main/java/goit/javadeveloper/Module15SpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package goit.javadeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module15SpringBootSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module15SpringBootSecurityApplication.class, args);
+	}
+
+}
